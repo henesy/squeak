5ebb436059c75a5f5811572cf282619fc9d4ac7c
Sean Hinchee <henesy@users.noreply.github.com>
2017-08-10 15:23:26 -0500

    Initial commit of port

---
+++ Plan9/src/vm/sqNamedPrims.h
@@ -0,0 +1,19 @@
+/* Smalltalk from Squeak4.5 with VMMaker 4.13.6 translated as C source on 11 December 2014 12:55:17 pm */
+/* This is an automatically generated table of all builtin modules in the VM */
+
+extern sqExport vm_exports[];
+extern sqExport os_exports[];
+extern sqExport B2DPlugin_exports[];
+extern sqExport BitBltPlugin_exports[];
+extern sqExport FilePlugin_exports[];
+extern sqExport SocketPlugin_exports[];
+
+sqExport *pluginExports[] = {
+	vm_exports,
+	os_exports,
+	B2DPlugin_exports,
+	BitBltPlugin_exports,
+	FilePlugin_exports,
+	SocketPlugin_exports,
+	NULL
+};
